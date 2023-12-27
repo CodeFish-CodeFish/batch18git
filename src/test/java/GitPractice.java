ab93a88b66ce401bfe950b80651d6387c9ddc33f
azamat73 <kaazamat73@gmail.com>
2023-12-26 19:28:37 -0600

    Azamat

--- src/test/java/GitPractice.java
+++ src/test/java/GitPractice.java
@@ -2,5 +2,6 @@ public class GitPractice {
     public static void main(String[] args) {
         System.out.println("Baaber");
         System.out.println("your name");
+        System.out.println("Azamat");
     }
 }
